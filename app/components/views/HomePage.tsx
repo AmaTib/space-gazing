e53bdd95fe4fc09b9135976cf8f33396422b8001
Amanda Tibell <141629199+AmaTib@users.noreply.github.com>
2024-12-02 15:26:33 +0100

    Refactor function for changing date

--- app/components/views/HomePage.tsx
+++ app/components/views/HomePage.tsx
@@ -17,9 +17,9 @@ const HomePage = ({ imgObject, todaysDate }: IHomePageProps) => {
   const [imgDate, setImgDate] = useState(imgObject.date);
   const [showForm, setShowForm] = useState(false);
 
-  const previousDate = async () => {
+  const changeDate = async (direction: number) => {
     const date = new Date(imgDate);
-    date.setDate(date.getDate() - 1);
+    date.setDate(date.getDate() + direction);
     const updatedDate = date.toISOString().split("T")[0]; //.split("T")[0] splits the string at the T and keeps only the first part
 
     const prevImage = await getImageByDate(updatedDate);
@@ -28,29 +28,28 @@ const HomePage = ({ imgObject, todaysDate }: IHomePageProps) => {
     setImgDate(prevImage.date);
   };
 
-  const nextDate = async () => {
-    const date = new Date(imgDate);
-    date.setDate(date.getDate() + 1);
-    const updatedDate = date.toISOString().split("T")[0];
-
-    const prevImage = await getImageByDate(updatedDate);
-
-    setImage(prevImage);
-    setImgDate(prevImage.date);
-  };
-
-  console.log(image.date, "&", todaysDate);
-
   return (
     <>
       <ImageOfTheDay imgObject={image} isImage={image.media_type === "image"} />
       <section>
-        <button onClick={previousDate}>Previous</button>
+        <button
+          onClick={() => {
+            changeDate(-1);
+          }}
+        >
+          Previous
+        </button>
         <p>{image.date}</p>
 
         {new Date(image.date).toDateString() !==
           new Date(todaysDate).toDateString() && (
-          <button onClick={nextDate}>Next</button>
+          <button
+            onClick={() => {
+              changeDate(1);
+            }}
+          >
+            Next
+          </button>
         )}
       </section>
 
