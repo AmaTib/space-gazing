28a40c938f5738c760da92dbe410f9aba4d45e4f
Amanda Tibell <141629199+AmaTib@users.noreply.github.com>
2024-12-03 15:01:30 +0100

    Functionality for searching date

--- app/components/Form.tsx
+++ app/components/Form.tsx
@@ -1,18 +1,42 @@
+"use client";
+
+import { ChangeEvent, FormEvent } from "react";
+
 interface IFormProps {
   inputType: string;
   btnText: string;
-  submit: () => void;
+  submit: (date: string) => void;
   close: () => void;
+  dateInput: string;
+  setDateInput: React.Dispatch<React.SetStateAction<string>>;
 }
 
-export const Form = ({ inputType, btnText, submit, close }: IFormProps) => {
+export const Form = ({
+  inputType,
+  btnText,
+  submit,
+  close,
+  dateInput,
+  setDateInput,
+}: IFormProps) => {
   return (
     <>
       <section>
         <button onClick={close}>X</button>
 
-        <form onSubmit={submit}>
-          <input type={inputType} />
+        <form
+          onSubmit={(e: FormEvent) => {
+            e.preventDefault();
+            submit(dateInput);
+          }}
+        >
+          <input
+            type={inputType}
+            value={dateInput}
+            onChange={(e: ChangeEvent<HTMLInputElement>) => {
+              setDateInput(e.target.value);
+            }}
+          />
           <button>{btnText}</button>
         </form>
       </section>
--- app/components/views/HomePage.tsx
+++ app/components/views/HomePage.tsx
@@ -1,11 +1,11 @@
 "use client";
 
-import { IImageInfo } from "@/app/models/IImageInfo";
-import ImageOfTheDay from "../ImageOfTheDay";
 import { useState } from "react";
+import { IImageInfo } from "@/app/models/IImageInfo";
 import { getImageByDate } from "@/services/imageServices";
 import { PrimaryButton } from "../PrimaryButton";
 import { Form } from "../Form";
+import ImageOfTheDay from "../ImageOfTheDay";
 
 interface IHomePageProps {
   imgObject: IImageInfo;
@@ -16,16 +16,28 @@ const HomePage = ({ imgObject, todaysDate }: IHomePageProps) => {
   const [image, setImage] = useState<IImageInfo>(imgObject);
   const [imgDate, setImgDate] = useState(imgObject.date);
   const [showForm, setShowForm] = useState(false);
+  const [dateInput, setDateInput] = useState(image.date);
 
   const changeDate = async (direction: number) => {
     const date = new Date(imgDate);
     date.setDate(date.getDate() + direction);
     const updatedDate = date.toISOString().split("T")[0]; //.split("T")[0] splits the string at the T and keeps only the first part
 
-    const prevImage = await getImageByDate(updatedDate);
+    const newImage = await getImageByDate(updatedDate);
 
-    setImage(prevImage);
-    setImgDate(prevImage.date);
+    setImage(newImage);
+    setImgDate(newImage.date);
+  };
+
+  const searchByDate = async (date: string) => {
+    console.log(date);
+
+    const newImage = await getImageByDate(date);
+    console.log(newImage);
+
+    setImage(newImage);
+    setImgDate(newImage.date);
+    setShowForm(false);
   };
 
   return (
@@ -57,12 +69,15 @@ const HomePage = ({ imgObject, todaysDate }: IHomePageProps) => {
         <Form
           btnText="&#128269;"
           inputType="date"
-          submit={() => {}}
+          submit={() => {
+            console.log(dateInput);
+            searchByDate(dateInput);
+          }}
           close={() => {
-            console.log("stängde");
-
             setShowForm(false);
           }}
+          dateInput={dateInput}
+          setDateInput={setDateInput}
         />
       ) : (
         <PrimaryButton
