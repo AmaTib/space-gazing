6274e755af74d206dcdb2d0c177f2d55bb5bb67a
Amanda Tibell <141629199+AmaTib@users.noreply.github.com>
2025-10-15 12:54:11 +0200

    API not working message

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,22 @@
-import { getImageByDate } from "@/services/imageServices";
-import HomePage from "./components/views/HomePage";
+/* import { getImageByDate } from "@/services/imageServices"; */
+/* import HomePage from "./components/views/HomePage"; */
 
 export const dynamic = "force-dynamic";
 
 export default async function Home() {
-  const date = new Date();
+  /*   const date = new Date();
   const day = String(date.getDate()).padStart(2, "0");
   const month = String(date.getMonth() + 1).padStart(2, "0");
   const year = date.getFullYear();
 
   const todaysDate = year + "-" + month + "-" + day;
 
-  const image = await getImageByDate(todaysDate);
+  const image = await getImageByDate(todaysDate); */
 
   return (
     <>
-      <HomePage imgObject={image} todaysDate={todaysDate} />
+      {/* <HomePage imgObject={image} todaysDate={todaysDate} /> */}
+      <h1>NASA APOD API not working at the moment</h1>
     </>
   );
 }
